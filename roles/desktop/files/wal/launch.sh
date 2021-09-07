140b8150a9da294df23106ee9baac58d2de149be
Tyler Rarick <tyler.rarick@gmail.com>
2021-09-07 13:04:30 -0500

    Changed up rofi and wal stuff

--- roles/desktop/files/wal/launch.sh
+++ roles/desktop/files/wal/launch.sh
@@ -1,28 +1,2 @@
 #!/usr/bin/env bash
 wal -i "$HOME/.config/feh/background.jpg" --backend colorthief -n
-
-# Source the pywal color file
-. "$HOME/.cache/wal/colors.sh"
-
-BGC=`printf "%s\n" "$background"`
-BG=${BGC:1}
-FGC=`printf "%s\n" "$foreground"`
-FG=${FGC:1}
-AC=`printf "%s\n" "$color1"`
-COLOR_ACTIVE=`printf "%s\n" "$color3"`
-COLOR_PASSIVE=`printf "%s\n" "$color4"`
-COLOR_URGENT=`printf "%s\n" "$color6"`
-
-# rofi
-RFILE="$HOME/.cache/wal/rofi-colors.rasi"
-cat > $RFILE << EOF
-/* colors */
-
-* {
-  al:   #000000FF;
-  bg:   #${BG}BF;
-  bga:  #${BG}FF;
-  fg:   #${FG}FF;
-  ac:   ${AC}FF;
-  se:   ${AC}1A;
-}
