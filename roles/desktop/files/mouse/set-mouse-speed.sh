13071287d9b65c40cf7ad7973a5d86bec2e8c4d9
Tyler Rarick <tyler.rarick@gmail.com>
2023-06-26 13:24:08 -0700

    Updated mouse speed

--- roles/desktop/files/mouse/set-mouse-speed.sh
+++ roles/desktop/files/mouse/set-mouse-speed.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-device="pointer:Logitech G305"
+devices=(
+  "pointer:Logitech G305"
+  "pointer:Logitech USB Receiver Mouse"
+)
 
 # Disable mouse acceleration
 # https://wiki.archlinux.org/title/Mouse_acceleration
-xinput set-prop "$device" "libinput Accel Profile Enabled" 0 1
+for device in "${devices[@]}"; do
+  xinput set-prop "$device" "libinput Accel Profile Enabled" 0 1
+done
